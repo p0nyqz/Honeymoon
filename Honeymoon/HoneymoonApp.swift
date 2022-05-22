a9f2cdd5b8706b40bfb829c710f37fc92b2faaeb
p0nyqz <ponyquiz@gmail.com>
2022-05-22 23:48:38 +0200

    Initial Commit

---
+++ Honeymoon/HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by p0nyqz on 22.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
