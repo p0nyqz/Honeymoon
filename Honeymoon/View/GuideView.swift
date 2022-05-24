41012e1511b8e5a089e8eed2167c03e18fdebd5f
p0nyqz <ponyquiz@gmail.com>
2022-05-24 23:01:58 +0200

    Guideline Screen added

---
+++ Honeymoon/View/GuideView.swift
@@ -0,0 +1,76 @@
+//
+//  GuideView.swift
+//  Honeymoon
+//
+//  Created by p0nyqz on 24.05.2022.
+//
+
+import SwiftUI
+
+struct GuideView: View {
+    var body: some View {
+        ScrollView(.vertical, showsIndicators: false) {
+            VStack(alignment: .center, spacing: 20) {
+                HeaderComponent()
+                
+                Spacer(minLength: 10)
+                
+                Text("Get Started!")
+                    .fontWeight(.black)
+                    .font(.largeTitle)
+                    .foregroundColor(Color.pink)
+                
+                Text("Discover and pick the perfect destination for your romantic Honeymoon!")
+                    .lineLimit(nil)
+                    .multilineTextAlignment(.center)
+                
+                Spacer(minLength: 10)
+                
+                VStack(alignment: .leading, spacing: 25) {
+                    GuideComponent(
+                        title: "Like",
+                        subtitle: "Swipe right",
+                        description: "Do you like this destination? Touch the screen and swipe right. It will be saved to the favourites.",
+                        icon: "heart.circle"
+                    )
+                    
+                    GuideComponent(
+                        title: "Dismiss",
+                        subtitle: "Swipe left",
+                        description: "Would you rather skip this place? Touch the screen and swipe left. You will no longer see it.",
+                        icon: "xmark.circle")
+                    }
+                    
+                GuideComponent(
+                    title: "Book",
+                    subtitle: "Tap the button",
+                    description: "Our selection of honeymoon resorts is perfect settings for you to embark you new life together",
+                    icon: "checkmark.square")
+                
+                Button(action: {
+                    print("A button was tapped.")
+                }) {
+                    Text("Continue".uppercased())
+                        .font(.headline)
+                        .padding()
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                        .background(Capsule().fill(Color.pink))
+                        .foregroundColor(Color.white)
+                }
+                
+                Spacer(minLength: 10)
+                
+            }
+            .frame(minWidth: 0, maxWidth: .infinity)
+            .padding(.top, 15)
+            .padding(.bottom, 25)
+            .padding(.horizontal, 25)
+        }
+    }
+}
+
+struct GuideView_Previews: PreviewProvider {
+    static var previews: some View {
+        GuideView()
+    }
+}
