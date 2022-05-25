e4c300d284d03b57129d1abe8f08c05bb9c093aa
p0nyqz <ponyquiz@gmail.com>
2022-05-25 22:18:17 +0200

    Insert and Remove Cards added

--- Honeymoon/View/ContentView.swift
+++ Honeymoon/View/ContentView.swift
@@ -15,10 +15,11 @@ struct ContentView: View {
     @State var showInfo: Bool = false
     @GestureState private var dragState = DragState.inactive
     private var dragAreaThreshold: CGFloat = 65.0
+    @State private var lastCardIndex: Int = 1
     
     // MARK: - CARD VIEWS
     
-    var cardViews: [CardView] = {
+    @State var cardViews: [CardView] = {
         var views = [CardView]()
         for index in 0..<2  {
             views.append(CardView(honeymoon: honeymoonData[index]))
@@ -26,7 +27,22 @@ struct ContentView: View {
         return views
     }()
     
+    // MARK: MOVE THE CARD
+    
+    private func moveCards() {
+        cardViews.removeFirst()
+        
+        self.lastCardIndex += 1
+        
+        let honeymoon = honeymoonData[lastCardIndex % honeymoonData.count]
+        
+        let newCardView = CardView(honeymoon: honeymoon)
+        
+        cardViews.append(newCardView)
+    }
+    
     // MARK: TOP CARD
+    
     private func isTopCard(cardView: CardView) -> Bool {
         guard let index = cardViews.firstIndex(where: { $0.id == cardView.id }) else {
             return false
@@ -111,6 +127,16 @@ struct ContentView: View {
                                     state = .dragging(translation: drag?.translation ?? .zero)
                                 default:
                                     break
+                                }
+                            })
+                            .onEnded({(value) in
+                                guard case .second(true, let drag?) = value else {
+                                    return
+                                }
+                                
+                                if drag.translation.width < -self.dragAreaThreshold ||
+                                    drag.translation.width > self.dragAreaThreshold {
+                                    self.moveCards()
                                 }
                             })
                     )
